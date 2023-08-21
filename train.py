ad2f3f55709c221306a2ad3fb49f09bd7dabc51f
Ihor Kostiuk <ihor.kostiuk@knu.ua>
2023-08-21 15:02:34 +0300

    Changed train logic

--- train.py
+++ train.py
@@ -4,12 +4,12 @@ from src.utils.callbacks.checkpoint import TrimmedModelCheckpoint
 import tensorflow as tf
 
 # Hyperparameters
-batch_size = 16
+batch_size = 200
 root_folder = ""
 
 # Loading Datasets
 train_dataset_loader = DatasetLoader(root_folder, volume='train')
-train_dataset = train_dataset_loader(image_size=(256, 256), shuffle=True, max=200000, batch_size=batch_size)
+train_dataset = train_dataset_loader(image_size=(256, 256), shuffle=True, batch_size=batch_size)
 
 validation_dataset_loader = DatasetLoader(root_folder, volume='val')
 validation_dataset = validation_dataset_loader(image_size=(256, 256), max=1000, shuffle=False)
@@ -19,7 +19,7 @@ alexnet = instantiate_model(input_shape=(256, 256, 3))
 print(alexnet.summary())
 
 alexnet.compile(
-    optimizer=tf.keras.optimizers.Adam(learning_rate=0.009),
+    optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
     loss=tf.keras.losses.SparseCategoricalCrossentropy(),
     metrics=['accuracy']
 )
@@ -27,12 +27,12 @@ alexnet.compile(
 history = alexnet.fit(
     train_dataset,
     batch_size=batch_size,
-    epochs=25,
+    epochs=90,
     callbacks=[TrimmedModelCheckpoint()],
     validation_data=validation_dataset
 )
 
 tf.print("Learning process is over!")
-tf.print("Friting history to the history-0.txt file")
+tf.print("Writing history to the history-0.txt file")
 with open('checkpoints/history-0.txt', 'w') as f:
     f.write(history)
